2ec85ae9e9c2c70e923404324fe7a97038ea654f
alex <alex.popa@gmail.com>
2015-10-15 10:13:58 -0400

    Previous checking missed these files

---
+++ CityHall/Synchronous/ISettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncISettings = CityHall.ISettings;
+
+namespace CityHall.Synchronous
+{
+    public interface ISettings
+    {
+        AsyncISettings AsynchronousSettings();
+    }
+}
