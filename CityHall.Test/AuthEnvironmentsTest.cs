e27614b9689549f3debdc5b50f175cbd31baef11
alex <alex.popa@gmail.com>
2015-10-26 18:07:10 -0400

    Previous checkin missed these files

---
+++ CityHall.Test/AuthEnvironmentsTest.cs
@@ -0,0 +1,47 @@
+﻿using CityHall.Responses;
+using CityHall.Synchronous;
+using Moq;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityHall.Test
+{
+    [TestFixture]
+    public class AuthEnvironmentsTest
+    {
+        /// <summary>
+        /// A user should be able to get details for an environment
+        /// </summary>
+        [Test]
+        public void GetEnvironments()
+        {
+            var resource = "auth/env/dev/";
+
+            var settings = TestSetup.SetupCall(TestSetup.Responses.DevEnvironment, Method.GET, resource);           
+            var env = settings.GetEnvironment("dev");
+            TestSetup.ErrorResponseHandled<EnvironmentResponse>(() => settings.GetEnvironment("dev"));
+            TestSetup.LoggedOutHonored(settings, () => settings.GetEnvironment("dev"));
+
+            Assert.AreEqual(env.Rights.Count(), TestSetup.Responses.DevEnvironment.Users.Count(), "The data received from the call should be visible in the call");
+        }
+
+        /// <summary>
+        /// A user should be able to create an environment
+        /// </summary>
+        [Test]
+        public void CreateEnvironment()
+        {
+            var resource = "auth/env/qa/";
+
+            var settings = TestSetup.SetupCall(TestSetup.Responses.Ok, Method.POST, resource);
+            settings.CreateEnvironment("qa");
+            TestSetup.ErrorResponseHandled<BaseResponse>(() => settings.CreateEnvironment("qa"));
+            TestSetup.LoggedOutHonored(settings, () => settings.CreateEnvironment("qa"));
+        }
+    }
+}
---
+++ CityHall/Responses/EnvironmentResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CityHall.Responses
+{
+    public class EnvironmentResponse : BaseResponse
+    {
+        public Dictionary<string, int> Users { get; set; }
+    }
+}
